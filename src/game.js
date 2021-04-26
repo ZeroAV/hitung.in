90af2e7ad6609eec073fc4b2eb2c29ebb458e52c
Av <aditya.alvari28@gmail.com>
2021-04-26 11:04:48 +0700

    Lowered music volume

--- src/game.js
+++ src/game.js
@@ -37,7 +37,7 @@ var check2 = document.getElementById("check2");
 var music = document.getElementById("music");
 var correctSound = document.getElementById("correctSound");
 correctSound.volume = 0.5;
-music.volume = 0.5;
+music.volume = 0.2;
 music.loop = true;
 music.play();
 
